3e274f6515edb5e3e066ad8a79848eb3c9cf1d06
unknown <bjoern>
2012-06-15 18:46:58 +0200

    Klasse HauptmaskeData

--- Zeiterfassung/Zeiterfassung/Classes/DataHauptmaske.cs
+++ Zeiterfassung/Zeiterfassung/Classes/DataHauptmaske.cs
@@ -22,8 +22,18 @@ namespace Zeiterfassung.Classes
        public List<string> getProjects(string user)
        {
            List<string> projects = new List<string>();
-           string sql = "SELECT * FROM tprojekt WHERE prID IN(SELECT prID FROM tmita_proj WHERE miID = " + user + ")";//sollte funzen oder?
-           MySqlCommand
+           string sql = "SELECT prName FROM tprojekt WHERE prID IN(SELECT prID FROM tmita_proj WHERE miID = " + user + ")";//sollte funzen oder?
+           MySqlCommand cmd = con.CreateCommand();
+           cmd.CommandText = sql;
+           MySqlDataReader reader;
+           con.Open();
+           reader = cmd.ExecuteReader();
+       
+          for(int i =0;reader.Read();i++)
+           {
+              projects.Add(reader.GetValue(i).ToString());
+           }
+
                return projects;
        }
     }
