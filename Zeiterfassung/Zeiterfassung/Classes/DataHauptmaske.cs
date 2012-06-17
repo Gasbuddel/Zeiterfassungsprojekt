4c65b8733a214a3be97840739f34f5b74a1067cb
bjoern <bjoern@ia210.com>
2012-06-17 09:56:11 +0200

    loadProjects() fertig

--- Zeiterfassung/Zeiterfassung/Classes/DataHauptmaske.cs
+++ Zeiterfassung/Zeiterfassung/Classes/DataHauptmaske.cs
@@ -10,6 +10,7 @@ namespace Zeiterfassung.Classes
     {
        private MySqlConnection con;
        private string user;
+       private Dictionary<int, string> projects;
 
        DataHauptmaske(string userid)//der Konstruktor kriegt die miID vom angemeldeten Nutzer
        {
@@ -19,23 +20,42 @@ namespace Zeiterfassung.Classes
             
        }
 
-       public List<string> getProjects(string user)
+       public void loadProjects(string user)
        {
-           List<string> projects = new List<string>();
-           string sql = "SELECT prName FROM tprojekt WHERE prID IN(SELECT prID FROM tmita_proj WHERE miID = " + user + ")";//sollte funzen oder?
+          
+           string sql = "SELECT prName, prID FROM tprojekt WHERE prID IN(SELECT prID FROM tmita_proj WHERE miID = " + user + ")";//sollte funzen oder?
            MySqlCommand cmd = con.CreateCommand();
+           
            cmd.CommandText = sql;
            MySqlDataReader reader;
            con.Open();
            reader = cmd.ExecuteReader();
        
+<<<<<<< HEAD
            for(int i =0;reader.Read();i++)
+=======
+          while(reader.Read())
+>>>>>>> loadProjects fertig
            {
-              projects.Add(reader.GetValue(i).ToString());
+              projects.Add(reader.GetInt32(1), reader.GetString(0));
            }
 
+<<<<<<< HEAD
            con.Close();
            return projects;
+=======
+          con.Close();    
+       }
+
+       public void loadBookings()
+       {
+           string sql = "Select";
+           MySqlCommand cmd = con.CreateCommand();
+           MySqlDataReader reader= new MySqlDataReader();
+           con.Open();
+           reader = cmd.ExecuteReader();
+
+>>>>>>> loadProjects fertig
        }
     }
 }
